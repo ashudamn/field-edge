2f39dbd8eec2f2c20336e84522bc65557bde0cb7
ashutosh kushwah <ashutoshkushwah2014@gmail.com>
2023-10-19 11:25:35 +0530

    added github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  api:'https://getinvoices.azurewebsites.net/api/',
+  getCustomersRoute:'Customers',
+  postCustomerRoute:'Customer',
+  deleteCustomerRoute:'Customer',
+  getCustomerByIdRoute:'Customer'
 };
